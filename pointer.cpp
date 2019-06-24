c402f8272754a573d2eb749f0607dc2b6ae6d489
Ronak Sharma <sharmaronak79@yahoo.com>
2019-06-24 19:42:13 -0400

    pointer.cpp

---
+++ pointer.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : pointer.cpp
+// Author      : ronak
+// Version     :
+// Copyright   : Your copyright notice
+// Description : use of pointer
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a=100;
+	int *ptr1;
+
+	ptr1=&a;
+
+
+	cout << "address of a is = " << &a << endl; // prints address of a
+	cout << "address of a is = " << ptr1 << endl; // prints address of a
+	cout << "value of a is = " << a << endl; // prints value of a
+	cout << "value of a is = " << *ptr1 << endl; // prints value of a, usng pointer
+
+	return 0;
+}
